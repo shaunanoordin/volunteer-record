b54ba5aacf07bbed77a85652c42f9b841dcd9c79
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2020-11-11 15:43:08 +0000

    Fetch project preferences

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,27 +1,51 @@
 import React from 'react'
-import { Box } from 'grommet'
+import { Box, Heading, Text } from 'grommet'
 import { observer } from 'mobx-react'
 import apiClient from 'panoptes-client/lib/api-client'
 
 import AppContext from 'stores'
 
+const ITEMS_PER_PAGE = 20  // Panoptes default
+
 function Stats () {
   const store = React.useContext(AppContext)
   
-  React.useEffect(() => {
-    apiClient.get('/project_preferences', {}).then(projectPreferences => {
-    
-      console.log('+++ project_preferences', projectPreferences)
+  const [projPrefs, setProjPrefs] = React.useState([])
+  
+  function fetchProjPrefs (page = 1, results = []) {
+    apiClient.get('/project_preferences', { page, page_size: ITEMS_PER_PAGE })
+    .then(items => {
+      
+      const updatedResults = [...results, ...items]
+      if (items.length >= ITEMS_PER_PAGE) {
+        fetchProjPrefs(page + 1, updatedResults)
+      } else {
+        finishFetch(updatedResults)
+      }
     })
-    
-    
+    .catch(err => {
+      console.error(err)
+    })
+  }
+  
+  function finishFetch (results) {
+    console.log('+++ FINISHED')
+    setProjPrefs(results)
+  }
+  
+  React.useEffect(() => {
+    console.log('+++ INITIATE')
+    fetchProjPrefs()
   }, [store])
   
   if (!store.auth.user) return null
   
   return (
     <Box>
-      STATS
+      <Heading use='h2'>Stats</Heading>
+      <Box>
+        <Text>Projects participated in: {projPrefs.length}</Text>
+      </Box>
     </Box>
   )
 }
